9cee87a09089a8e5cdd9a8cb81e887b858110471
DDeis <social.deis@gmail.com>
2013-09-14 20:56:31 +0200

    Fixed callback problem

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -3,7 +3,7 @@
 /* App Module */
 
 var servicesModule = angular.module('services', ['ngResource']);
-var filtersModule = angular.module('filters', []);
+var filtersModule = angular.module('filters', [])
 
 var esieAddressApp = angular.module('esieAddress', ['filters', 'services']);
 
@@ -12,10 +12,11 @@ var path = "http://"+url+"/rest";
 
 esieAddressApp.config(['$routeProvider', function ($routeProvider) {
 	$routeProvider.
+		when('/', {redirectTo: '/contacts'}).
 
 		/* Related Contacts Pages */
 		when('/contacts', {templateUrl: 'partials/contact-list.html', controller: ContactListCtrl}).
 
-		otherwise({redirectTo: '/contacts'});
+		otherwise({redirectTo: '/error/404'});
 }]);
 
--- src/main/webapp/js/controllers/contactListController.js
+++ src/main/webapp/js/controllers/contactListController.js
@@ -7,7 +7,7 @@ function ContactListCtrl($scope, $location, Contact) {
 	Contact.query(function(data) {
 		$scope.contacts = data;
 	}, function(error) {
-		$location.path('/error/'+error.status);
+		// $location.path('/error/'+error.status);
 	});
 
 }
