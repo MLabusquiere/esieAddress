78c6466c105f38b1afec69baa4cfb76058e548c6
MLabusquiere <labusquiere@et.esiea.fr>
2013-10-02 18:35:04 +0200

    Get the fbtoken access

--- src/main/java/fr/esiea/esieaddress/controllers/login/FBLoggin.java
+++ src/main/java/fr/esiea/esieaddress/controllers/login/FBLoggin.java
@@ -23,11 +23,18 @@ package fr.esiea.esieaddress.controllers.login;
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
+import com.sun.tools.internal.xjc.reader.xmlschema.bindinfo.BIConversion;
+import org.apache.commons.httpclient.DefaultHttpMethodRetryHandler;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.httpclient.params.HttpMethodParams;
+import org.apache.http.HttpException;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import org.springframework.web.bind.annotation.RequestParam;
+import org.apache.commons.httpclient.methods.GetMethod;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -39,8 +46,8 @@ public class FBLoggin {
 
     private final String FACEBOOK_APP_ID = "208719949301687";
     private final String FACEBOOK_REDIRECT_URL = "http://localhost:8080/esieAddress/rest/login/facebookRedirect";
-    private final String FACEBOOK_EXCHANGE_KEY="5a7150fef9431de5a4ab5b6867c0be2b";
-
+    private final String FACEBOOK_EXCHANGE_KEY="azertyuiop";
+    private final String FACEBOOK_SECRET_KEY="5a7150fef9431de5a4ab5b6867c0be2b";
 
     @RequestMapping(value = "/facebookAuthentication", method= RequestMethod.GET)
     public void getFacebookLogin(HttpServletRequest request,HttpServletResponse response) {
@@ -60,8 +67,75 @@ public class FBLoggin {
     }
 
     @RequestMapping(value = "/facebookRedirect", method= RequestMethod.GET)
-    public void getFacebookLogin() {
+    public void getFacebookLogin(@RequestParam("code") String code) {
         LOGGER.info("Receive a request from facebook");
+        if( ! code.isEmpty())    {
+            //If we received a valid code, we can continue to the next step
+            //Next we want to get the access_token from Facebook using the code we got,
+            //use the following url for that, in this url,
+            //client_id-our app id(same as above),  redirect_uri-same as above, client_secret-same as                //above, code-the code we just got
+            String url="https://graph.facebook.com/oauth/access_token?"
+                    + "client_id=" + FACEBOOK_APP_ID
+                    + "&redirect_uri=" + FACEBOOK_REDIRECT_URL
+                    + "&client_secret=" + FACEBOOK_SECRET_KEY
+                    + "&code=" + code;
+            // Create an instance of HttpClient.
+            HttpClient client = new HttpClient();
+            // Create a method instance.
+            GetMethod method = new GetMethod(url);
+            // Provide custom retry handler is necessary
+            method.getParams().setParameter(HttpMethodParams.RETRY_HANDLER,
+                    new DefaultHttpMethodRetryHandler(3, false));
+            try {
+                // Execute the method.
+                int statusCode = client.executeMethod(method);
+                if (statusCode != HttpStatus.SC_OK) {
+                    System.err.println("Method failed: " + method.getStatusLine());
+                }
+                // Read the response body.
+                byte[] responseBody = method.getResponseBody();
+                // Deal with the response.Use caution: ensure correct character encoding and is
+                // not binary data
+                String responseBodyString=new String(responseBody);
+                //will be like below,                                 //access_token=AAADD1QFhDlwBADrKkn87ZABAz6ZCBQZ//DZD&expires=5178320
+                //now get the access_token from the response
+                if(responseBodyString.contains("access_token")){
+                    //success
+                    String[] mainResponseArray=responseBodyString.split("&");
+                    //like //{"access_token= AAADD1QFhDlwBADrKkn87ZABAz6ZCBQZ//DZD ","expires=5178320"}
+                    String accesstoken="";
+                    for (String string : mainResponseArray) {
+                        if(string.contains("access_token")){
+                            accesstoken=string.replace("access_token=", "").trim();
+                        }
+                    }
 
+                    LOGGER.info("Congratulation " + accesstoken);
+
+                       /*
+                       //now we have the access token :)
+                       //Great. Now we have the access token, I have used restfb to get the user details here
+                       FacebookClient facebookClient = new DefaultFacebookClient(accesstoken);
+                       User user = facebookClient.fetchObject("me", User.class);
+                       //In this user object, you will have the details you want from Facebook,  Since we have    the  access token with us, can play around and see what more can be done
+                       //CAME UP TO HERE AND WE KNOW THE USER HAS BEEN AUTHENTICATED BY FACEBOOK, LETS AUTHENTICATE HIM IN OUR APPLICATION
+                       //NOW I WILL CALL MY doAutoLogin METHOD TO AUTHENTICATE THE USER IN MY SPRING SECURITY CONTEXT
+                       */
+                }else{
+                    //failed
+                    return;
+                }
+
+            } catch (IOException e) {
+                System.err.println("Fatal transport error: " + e.getMessage());
+                e.printStackTrace();
+            } finally {
+                // Release the connection.
+                method.releaseConnection();
+            }
+        }else{
+            //failed
+            return;
+        }
     }
 }
