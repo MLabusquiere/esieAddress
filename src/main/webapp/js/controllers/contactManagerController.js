1a0c1e31400e0476397974ff1d1cb32066a99044
DDeis <social.deis@gmail.com>
2013-11-04 18:38:22 +0100

    Fixing jQuery

--- src/main/webapp/js/controllers/contactManagerController.js
+++ src/main/webapp/js/controllers/contactManagerController.js
@@ -4,55 +4,61 @@
 
 var module = angular.module('esieAddress.controllers');
 
-module.controller('ContactManagerCtrl', function ($rootScope, $location, $scope, Contact) {
+module.controller('ContactManagerCtrl', function ($rootScope, $location, $scope, $timeout, Contact) {
 
 	$scope.contacts = [];
 
 	$scope.modal = "";
 
-	var loadContacts = function(){
-		Contact.query(function (data) {
-			$scope.contacts = data;
-			console.log("Contact list loaded successfully");
-		}, function (error) {
-			console.log("Error " + error.status);
-			$location.path('/error/' + error.status);
-		});
+	var loadContacts = function () {
+		Contact.query(
+			function (data) {
+				$scope.contacts = data;
+				console.log("Contact list loaded successfully");
+				$timeout(sizeContent(), 1000);
+			},
+			function (error) {
+				console.log("Error " + error.status);
+				$location.path('/error/' + error.status);
+			}
+		);
 	}
 
 	loadContacts();
 
-	$scope.deleteContact = function() {
+	$scope.deleteContact = function () {
 		$('#modal').modal('hide');
 		for (var i = 0; i < $scope.contacts.length; i++) {
-			if($scope.contacts[i].selected == true){
-				Contact.remove({
-					id: $scope.contacts[i].id
-				}, function () {
-					$rootScope.$broadcast('updateContactList');
-				}, function (error) {
-					console.log(error);
-					$location.path('/error/' + error.status);
-				});
+			if ($scope.contacts[i].selected == true) {
+				Contact.remove(
+					{id: $scope.contacts[i].id},
+					function () {
+						$rootScope.$broadcast('updateContactList');
+					},
+					function (error) {
+						console.log(error);
+						$location.path('/error/' + error.status);
+					}
+				);
 			}
 		}
 		loadContacts();
 	}
 
-	$scope.contactVisibility = function(action) {
+	$scope.contactVisibility = function (action) {
 		for (var i = 0; i < $scope.contacts.length; i++) {
-			if($scope.contacts[i].selected == true){
+			if ($scope.contacts[i].selected == true) {
 				$scope.contacts[i].actif = false;
-				if(action == "show"){
+				if (action == "show") {
 					$scope.contacts[i].actif = true;
 				}
 				$scope.contacts[i].selected = undefined;
-				Contact.updateVisibility({id: $scope.contacts[i].id, visibility:$scope.contacts[i].actif}
-				, function () {
-					$rootScope.$broadcast('updateContactList');
-				}, function (error) {
-					console.log(error);
-				});
+				Contact.updateVisibility({id: $scope.contacts[i].id, visibility: $scope.contacts[i].actif}
+					, function () {
+						$rootScope.$broadcast('updateContactList');
+					}, function (error) {
+						console.log(error);
+					});
 			}
 		}
 	}
--- src/main/webapp/js/functions.js
+++ src/main/webapp/js/functions.js
@@ -5,20 +5,37 @@ $(document).ready(sizeContent);
 
 //Every resize of window
 $(window).resize(sizeContent);
+$(document).resize(sizeContent);
+$("#content").resize(sizeContent);
 
-$("#sidebar-header").resize(sizeContent);
+$("#sidebar-header").resize(sizeContent());
+
+$("#cfg-btn").click(function() {
+	sizeContent();
+	console.log("cfg");
+});
 
 //Dynamically assign height
 function sizeContent() {
-	var pageHeight = $(document).height();
-	var sidebarHeaderHeight = $("#sidebar-header").height();
-//	var pageHeaderHeight = $("#page-header").innerHTML;
-	var pageHeaderHeight = 150;
+	var pageHeight = $(window).height() - 20;
+	var contentHeight = $("#content").outerHeight(true);
+	var pageContentHeight = $("#pageContent").outerHeight(true);
+	var sidebarHeaderHeight = $("#sidebar-header").outerHeight(true);
+//	var pageHeaderHeight = $(".page-header");
+//	var pageHeaderHeight = 150;
+	console.log($(".page-header").outerHeight(true));
 
-	$("#sidebar-left").css("height", pageHeight + "px");
-	$("#contacts").css("height", pageHeight - sidebarHeaderHeight - 30 + "px");
-	$("#content").css("height", pageHeight + "px");
-	$("#pageContent").css("height", pageHeight - pageHeaderHeight + "px");
+//	$("#sidebar-left").height(pageHeight);
+	console.log(pageHeight, pageContentHeight);
+	if(contentHeight > pageHeight) {
+		$("#contacts").height(contentHeight - sidebarHeaderHeight);
+	}
+	else {
+		$("#contacts").height(pageHeight - sidebarHeaderHeight - 20);
+	}
+//	$("#content").height(pageHeight - 22);
+//	$("#pageContent").height(pageHeight - pageHeaderHeight);
+//	$(document).height(pageHeight);
 }
 
 $(function (){
